224f51ac58d6cef1a8a63c27a741813f5d5034e9
Atyla da Silva <atylamendes97@gmail.com>
2023-10-27 15:39:00 -0300

    Updated API call side-effect
    The response from the API call is now saved in session storage. If the response is already stored in session storage, no further API calls will be made.

--- src/App.js
+++ src/App.js
@@ -50,18 +50,42 @@ function App() {
   });
 
   /* Side-Effects */
+
+  /**
+   * Makes an API call to 'https://api.imgflip.com/get_memes' and saves the response to session storage. Also updates state for memesArray and memesState with the response from the API call.
+  */
   useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then(response => response.json())
-      .then(response => {
-        setMemesArray(response.data.memes);
-        setMemeState(currentState => {
-          return {
-            ...currentState,
-            memeURL: getRandomMemeURL(response.data.memes)
-          }
+    //Check if the response from the API call is stored in session storage
+    if (sessionStorage.getItem('memes') == null) {
+      //Make an API call
+      fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(response => {
+          //Save response in session storage
+          sessionStorage.setItem('memes', JSON.stringify(response.data.memes));
+
+          //Update state
+          setMemesArray(response.data.memes);
+          setMemeState(currentState => {
+            return {
+              ...currentState,
+              memeURL: getRandomMemeURL(response.data.memes)
+            }
+          });
         });
-      });
+    } else { 
+      //Get response from session storage
+      const memes = JSON.parse(sessionStorage.getItem('memes'));
+      
+      //Update state
+      setMemesArray(memes);
+      setMemeState(currentState => { 
+        return {
+          ...currentState,
+          memeURL: getRandomMemeURL(memes)
+        }
+      })
+    }
   }, []);
   
   /* Render */
